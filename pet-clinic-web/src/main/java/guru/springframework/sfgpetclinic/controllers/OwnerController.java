7793aeb59e6c964d7b5c852ee6ca2c1997c56e56
Cyouffy <cyouffy@gmail.com>
2023-06-09 17:16:49 +0300

    Adding an ability to add or update visit closes #38

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -51,7 +51,8 @@ public class OwnerController {
         }
 
         // find owners by last name
-        List<Owner> results = ownerService.findOwnerByLastNameLike("%" + owner.getLastName() + "%");
+        List<Owner> results = ownerService.findOwnerByLastNameLike("%"+ owner.getLastName() + "%");
+
         if (results.isEmpty()) {
             // no owners found
             result.rejectValue("lastName", "notFound", "not found");
@@ -63,7 +64,7 @@ public class OwnerController {
         } else {
             // multiple owners found
             model.addAttribute("selections", results);
-            return "/owners/ownersList";
+            return "owners/ownersList";
         }
     }
 
