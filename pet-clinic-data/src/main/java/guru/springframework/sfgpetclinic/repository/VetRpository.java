40d620f9e267c6bf62d9208dc3332f75a1991bd2
Cyouffy <cyouffy@gmail.com>
2023-08-13 16:57:56 +0300

    Adding Caching on Vets. Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VetRpository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VetRpository.java
@@ -1,7 +1,15 @@
 package guru.springframework.sfgpetclinic.repository;
 
 import guru.springframework.sfgpetclinic.model.Vet;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+import java.util.Collection;
+
 public interface VetRpository extends CrudRepository<Vet, Long> {
+    @Transactional
+    @Cacheable("vets")
+    Collection<Vet> findAll() throws DataAccessException;
 }
